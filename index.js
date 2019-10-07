c5fbbb1113fbc05314fcdc4a8f22d9ac4252ebe6
Double-A-92 <amato.amedeo@gmail.com>
2019-10-07 23:58:38 +0200

    Add Jaccard index algorithm (#3)

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var levenshtein = require('./levenshtein.js');
 var jaroDistance = require('./jaro.js');
 var jaroWinklerDistance = require('./jarowinkler.js');
 var naive = require('./naive.js');
+var jaccard = require('./jaccard.js');
 
 x = 'david';
 y = 'daved';
 
-console.log("Naive Match: " + naive(x,y));
-console.log("Hamming: " + hammingDistance(x,y));
-console.log("Levensthein: " + levenstheinDistance(x,y));
-console.log("Jaro Distance: " + jaroDistance(x,y));
-console.log("Jaro Winkler: " + jaroWinklerDistance(x,y));
-
-
+console.log("Naive Match: " + naive(x, y));
+console.log("Hamming: " + hammingDistance(x, y));
+console.log("Levensthein: " + levenstheinDistance(x, y));
+console.log("Jaro Distance: " + jaroDistance(x, y));
+console.log("Jaro Winkler:" + jaroWinklerDistance(x, y));
+console.log("Jaccard similarity coefficient:" + jaccard(x, y));
